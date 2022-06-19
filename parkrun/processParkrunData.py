e685a5ce4ef44b808fde220d5bbdb4333278125c
James Lester <jameslester78@gmail.com>
2022-06-19 14:00:43 +0100

    removed distance calculation stuff

--- parkrun/processParkrunData.py
+++ parkrun/processParkrunData.py
@@ -1,20 +1,3 @@
-'''
-todo:
-
-sqllite path parameter
-move the email function, wait for internet function to a seperate utility file
-
-DONE:
-detect new courses
-alert new courses
-multiple homes
-exclude list
-automate downloading json
-automate daily execution
-back up json - dropbox
-wait for internet connection
-'''
-
 import sys
 sys.path.append(r'C:\Users\james\Documents\python') #location of sendEmail.py
 
@@ -24,50 +7,6 @@ from pandas import json_normalize
 from geopy.distance import distance
 import sqlite3,urllib.request,socket,time
 from sendEmail import sendEmail
-
-#ONE OF THE FOLLOWING LOCATIONS SHOULD BE SELECTED AS HOME IN ORDER TO FIND THE CLOSEST AVAILABLE PARKRUN
-#IF YOU ADD A NEW HOME ALSO ADD THE COORDINATES
-
-#runfrom = 'codsall'
-runfrom = 'borehamwood'
-
-if runfrom == 'codsall':
-    home = (52.6237742,-2.1985467)
-elif runfrom == 'borehamwood':
-    home = (51.659428688873305, -0.27137887684018747)
-
-
-#MANUALLY ADD COMPLETED RUNS HERE
-completed = ['aldenham','canonspark','sunnyhill','wolverhamptonNew','wolverhampton','eastpark','dudley','walsall','telford','chasewater','cannockchase','sandwellvalley','isabeltrail'\
-            ,'severnvalleycountry','perryhall','woodgatevalleycountrypark','suttonpark','edgbastonreservoir','beacon','cannonhill','oaklands','babbsmill','kingsburywater','brueton','shrewsbury'
-            ,'oakhill','allypally','cassiobury','southoxhey','stalbans','hampsteadheath']
-
-# def send_email(run_list):
-
-#     '''send the email, you'll need to create a pickle file with gmail account and password in it, code to create file down the bottom'''
-
-#     import smtplib, ssl
-#     import pickle
-
-
-#     with open('C:\\Users\\james\\Documents\\python\\vars.pkl','rb') as f: 
-#         password, email = pickle.load(f)
-
-#     print(email)     
-
-#     port = 465  # For SSL
-#     smtp_server = "smtp.gmail.com"
-#     sender_email = email
-#     receiver_email = email
-#     password = password
-#     message = f"""Subject: New Parkrun
-
-#     There is a new Parkrun: {run_list}"""
-
-#     context = ssl.create_default_context()
-#     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-#         server.login(sender_email, password)
-#         server.sendmail(sender_email, receiver_email, message)
 
 def isconnected():
 
@@ -94,57 +33,46 @@ with open(jsonPath) as f:
 
 q = json_normalize(d['events'],record_path = 'features')
 
-q=q[(q["properties.countrycode"]==97) & (q["properties.seriesid"]==1) ][["id","geometry.coordinates","properties.eventname"]] 
+q=q[(q["properties.countrycode"]==97) & (q["properties.seriesid"]==1) ][["id","properties.eventname"]] 
 #filtering dataframe rows and specify cols
 #countrycode 97 = uk, seriesid =1 - non junior parkrun
 
-q[['c1','c2']] = pd.DataFrame(q["geometry.coordinates"].tolist(),index=q.index) #split out list column into seperate cols
-q['dist']=q.apply(lambda row: distance((row['c2'],row['c1']),home).miles,axis=1) #create a new column
-q=q.sort_values(by=['dist']) #sort dataframe
-q=q.drop(['geometry.coordinates','c1','c2'],axis=1) #remove columns we dont care about
-q=q.rename(columns={'dist':'distFromHome','properties.eventname':'eventName'}) #rename columns
+q=q.rename(columns={'properties.eventname':'eventName'}) #rename column
 
 
 conn = sqlite3.connect('C:\\Users\\james\\Dropbox\\temp\\parkrun.db')#create db
 c=conn.cursor()#create connection
 q.to_sql('stg_runs',conn, if_exists='replace')#upload data
 
-#print(pd.read_sql('select * from stg_runs',conn))#view data
-
 createTable ='\
 CREATE TABLE IF NOT EXISTS runs (\
     id           INTEGER,\
     eventName    TEXT,\
-    distFromHome REAL,\
     timestamp    DATETIME DEFAULT CURRENT_TIMESTAMP\
 );'
 
 c.execute(createTable)
 
-#this view will tell me which is the closest unrun course to my home
 createView = '\
 CREATE VIEW IF NOT EXISTS newEvents \
 AS \
 \
-select a.eventName,b.distFromHome from( \
+select a.eventName from( \
 select eventName from runs where timestamp = (select max(timestamp) from runs) \
 except \
 select eventName from runs where timestamp = (select max(timestamp) from runs where timestamp != (select max(timestamp) from runs))) a \
 inner join (select * from runs where timestamp = (select max(timestamp) from runs)) b on a.eventname = b.eventname\
 '
 
-#print(createView)
 c.execute(createView)
 
 insertTable = '\
-INSERT INTO runs ( id,eventname,distfromHome)\
-SELECT id,eventname,distfromHome FROM stg_runs\
+INSERT INTO runs ( id,eventname)\
+SELECT id,eventname FROM stg_runs\
 '
 
 c.execute(insertTable) #insert the processed data into database
 conn.commit()
-
-c.execute('select * from newEvents') #I think this can be removed
 
 newEventsDF = pd.read_sql('select * from newEvents',conn)
 
@@ -156,17 +84,4 @@ if neweventCount >0:
     sendEmail('New parkrun',run_list)
 
 
-
-closestEvents=q[~q["eventName"].isin(completed)] # ~ means NOT
-print(closestEvents.head(5)) #dump the 5 nearest to the screen
-
-
-'''
-create your password file
-
-password = 'your email password'
-email = 'your email address'
-
-with open('vars.pkl','wb') as f:
-    pickle.dump([password,email],f)
-'''            
+time.sleep(5) #delay to allow inspection for error messages
